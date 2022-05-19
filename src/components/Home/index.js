1c0905a38fa08d8bbc516b20fe94554f62de286e
kiran kumar <kirankumar76k>
2022-05-19 05:33:46 +0000

    add-background

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
         ) : (
           <div className="home-container">
             <InstaStories />
-            <hr className="hr-line" />
+
             <InstaPosts />
           </div>
         )}
